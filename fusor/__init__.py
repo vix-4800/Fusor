ec398948384a7245577f29dcb812086167823ce4
Anton Vix <Gordan.ei@protonmail.ch>
2025-07-18 04:06:25 +0400

    AUTHOR and ver update

--- fusor/__init__.py
+++ fusor/__init__.py
@@ -1,7 +1,7 @@
 APP_NAME = "Fusor"
-__version__ = "0.1.0"
+__version__ = "0.2.5"
 DESCRIPTION = (
     "A minimal PyQt6 application with helper tools for PHP development."
 )
-AUTHOR = "Fusor Dev"
+AUTHOR = "Anton Vix"
 
